e8fadb6f84ec42dcf34a07fa7d2a3c4acfa24478
Prabuddha16 <prabuddha.kumar_cs19@gla.ac.in>
2021-01-18 19:26:06 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Welcome to Java World!");
+        System.out.println("Master Prabuddha Kumar");
+
+    }
+}
